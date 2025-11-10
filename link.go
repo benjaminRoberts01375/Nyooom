f785d51cf5d4301f3f3b3474f8d5af29a62d4a0b
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-11-10 15:49:26 -0500

    Prevent : / and \\ from being in URL slugs

--- link.go
+++ link.go
@@ -36,6 +36,9 @@ func newLink(slug string, url string) (Link, error) {
 	// Check if slug and URL are set
 	if len(slug) < 3 || // Slug must be at least 3 characters
 		strings.Contains(slug, " ") || // Slug cannot contain spaces
+		strings.Contains(slug, ":") || // Slug cannot contain colons
+		strings.Contains(slug, "/") || // Slug cannot contain forward slashes
+		strings.Contains(slug, "\\") || // Slug cannot contain backslashes
 		strings.Contains(url, " ") || // URL cannot contain spaces
 		!strings.Contains(url, ".") || // URL must contain a dot
 		len(url) < 5 { // URL must be at least 5 characters
